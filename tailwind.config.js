80be165418a6b1684ff867dd1ec58ab2fbf434db
Hisam-Uddin-Shawon <hisamuddin599@gmail.com>
2024-12-05 14:55:22 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"], // Adjust this path as needed
+    theme: {
+      extend: {
+        backgroundImage: {
+          'custom-gradient': 'linear-gradient(232.96deg, #0E0E10 27.06%, rgba(23, 23, 26, 0.23) 92.12%)',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
